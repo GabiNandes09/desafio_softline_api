ead185e969af01c0f2dad342b36f998204cf0bbb
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-06-22 15:42:58 -0300

    feat: basic CRUD on controllers

--- Desafio_SoftLine_API/src/main/java/com/gabrielfernandes/Desafio_SoftLine/web/controller/ClientController.java
+++ Desafio_SoftLine_API/src/main/java/com/gabrielfernandes/Desafio_SoftLine/web/controller/ClientController.java
@@ -1,10 +1,22 @@
 package com.gabrielfernandes.Desafio_SoftLine.web.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabrielfernandes.Desafio_SoftLine.models.client.ClientModel;
 import com.gabrielfernandes.Desafio_SoftLine.service.ClientService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
@@ -13,4 +25,41 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/clients")
 public class ClientController {
     private final ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<List<ClientModel>> getAll(){
+        List<ClientModel> response = clientService.selectAll();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientModel> selectById(@PathVariable int id){
+        ClientModel response = clientService.selectById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientModel> create(
+        @RequestBody @Valid ClientModel client
+    ) {
+        ClientModel response = clientService.save(client);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientModel> update(
+        @PathVariable int id,
+        @RequestBody @Valid ClientModel client
+    ) {
+        ClientModel response = clientService.update(id, client);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteById(
+        @PathVariable int id
+    ) {
+        clientService.deleteById(id);
+        return ResponseEntity.ok("Cliente removido com sucesso");
+    }
 }
--- Desafio_SoftLine_API/src/main/java/com/gabrielfernandes/Desafio_SoftLine/web/controller/ProductController.java
+++ Desafio_SoftLine_API/src/main/java/com/gabrielfernandes/Desafio_SoftLine/web/controller/ProductController.java
@@ -1,10 +1,22 @@
 package com.gabrielfernandes.Desafio_SoftLine.web.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabrielfernandes.Desafio_SoftLine.models.product.ProductModel;
 import com.gabrielfernandes.Desafio_SoftLine.service.ProductService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
@@ -13,4 +25,41 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductModel>> getAll(){
+        List<ProductModel> response = productService.selectAll();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductModel> selectById(@PathVariable int id){
+        ProductModel response = productService.selectById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductModel> create(
+        @RequestBody @Valid ProductModel product
+    ) {
+        ProductModel response = productService.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductModel> update(
+        @PathVariable int id,
+        @RequestBody @Valid ProductModel product
+    ) {
+        ProductModel response = productService.update(id, product);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteById(
+        @PathVariable int id
+    ) {
+        productService.deleteById(id);
+        return ResponseEntity.ok("Produto removido com sucesso");
+    }
 }
