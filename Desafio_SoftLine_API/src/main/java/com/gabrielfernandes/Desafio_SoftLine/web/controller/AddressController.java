10af1a4d680aaea8711fa2b03c4f67ce3b2b8985
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-06-22 11:40:06 -0300

    feat: basic controllers

---
+++ Desafio_SoftLine_API/src/main/java/com/gabrielfernandes/Desafio_SoftLine/web/controller/AddressController.java
@@ -0,0 +1,7 @@
+package com.gabrielfernandes.Desafio_SoftLine.web.controller;
+
+import com.gabrielfernandes.Desafio_SoftLine.service.AddressService;
+
+public class AddressController {
+    private final AddressService addressService;
+}
---
+++ Desafio_SoftLine_API/src/main/java/com/gabrielfernandes/Desafio_SoftLine/web/controller/ProductController.java
@@ -0,0 +1,7 @@
+package com.gabrielfernandes.Desafio_SoftLine.web.controller;
+
+import com.gabrielfernandes.Desafio_SoftLine.service.ProductService;
+
+public class ProductController {
+    private final ProductService productService;
+}
